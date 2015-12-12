1134023fab9dfec34b0a06e2a00cb8279512d3d0
Matt Edelman <mattedelman@gmail.com>
2015-12-11 16:40:20 -0800

    remove console.log

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ var path = require('path');
 module.exports = function (context, callback) {
     context.srcPath = path.join(context.srcRoot, 'templates', context.name);
     context.name = 'templates/' + context.name ;
-    console.log('context', context);
     callback(null, context);
 };
